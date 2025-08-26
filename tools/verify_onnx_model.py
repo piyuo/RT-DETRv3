6f33cfe7aea7e1cd910a695f2e8dacdd56841ff4
piyuo <piyuo.com@gmail.com>
2025-08-26 15:33:34 -0700

    feat: update COCO validation dataset configuration and add utility for dataset reduction

--- tools/verify_onnx_model.py
+++ tools/verify_onnx_model.py
@@ -153,7 +153,7 @@ def verify_model(onnx_model, image_path, output_path, conf_threshold=0.5):
 if __name__ == "__main__":
     verify_model(
         #onnx_model="output/rtdetrv3_r18vd_6x_optimized.onnx",
-        onnx_model="output/olive/model/rtdetrv3_r18vd_6x.onnx",
+        onnx_model="output/olive/model.onnx",
         image_path="demo/demo.jpg",
         output_path="output/demo.jpg",
         conf_threshold=0.5
